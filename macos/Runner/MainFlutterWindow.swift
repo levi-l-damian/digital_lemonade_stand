3c36c505baf3a9967391d6c1bad9867a6be54734
levi-l-damian <levi.l.damian@gmail.com>
2025-11-03 13:15:14 -0500

    Limited macOS screen to 800x600

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -6,7 +6,18 @@ class MainFlutterWindow: NSWindow {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.minSize = NSSize(width: 800, height: 600)
+    if windowFrame.size.width < 800 || windowFrame.size.height < 600 {
+      let adjustedFrame = NSRect(
+        x: windowFrame.origin.x,
+        y: windowFrame.origin.y,
+        width: max(windowFrame.size.width, 800),
+        height: max(windowFrame.size.height, 600)
+      )
+      self.setFrame(adjustedFrame, display: true)
+    } else {
+      self.setFrame(windowFrame, display: true)
+    }
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
